6e4648f01e6d226e51ef6b4135da969668143a73
Ankit Chauhan <chauhana595@gmail.com>
2026-05-26 03:40:46 +0530

    config.ts -> config.tsx

---
+++ src/app/config.tsx
@@ -0,0 +1,11 @@
+// import { ROUTES } from '@/core/constants/routes'
+import type { AppRouteConfig } from '@app/router'
+
+export const appRouteConfig: AppRouteConfig = {
+  publicRoutes: [
+    // { path: ROUTES.app.dashboard, element: <DashboardPage /> },
+  ],
+  guestRoutes: [
+    // { path: ROUTES.auth.login, element: <LoginPage /> },
+  ],
+}
